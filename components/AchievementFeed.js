36d241be2f5d459468a00b94073f68d21f815f54
Bartosz Gałek <bartosz@galek.com.pl>
2023-02-24 00:08:57 +0100

    better feed

--- components/AchievementFeed.js
+++ components/AchievementFeed.js
@@ -1,6 +1,7 @@
 import { Avatar, Container, Progress, Text, Timeline } from "@mantine/core";
 import { API_URL, feed } from "../utils/queries";
 import { useQuery } from "react-query";
+import { formatDistanceToNow } from 'date-fns'
 
 export function AchievementFeed() {
     const { isLoading, data } = useQuery('achievements-feed', feed);
@@ -9,19 +10,24 @@ export function AchievementFeed() {
     return (
         <Container py="xl">
             <Timeline active={1} bulletSize={24} lineWidth={2}>
-                {data.map(x => Object.entries(x)).map(entry => (
-                    <Timeline.Item key={`${entry[0][0]}-${entry[0][1]}`}
-                                   bullet={
-                                       <Avatar
-                                           size={32}
-                                           radius="xl"
-                                           src={`${API_URL}/img/${entry[0][1]}@6x.png`}
-                                       />}>
-                        <Text color="dimmed"
-                              size="sm">{entry[0][0]} unlocked <strong>{entry[0][1]}</strong> achievement</Text>
-                        <Text size="xs" mt={4}>2 hours ago</Text>
-                    </Timeline.Item>
-                ))}
+                {data.map(entry => {
+                    const achievementDefinition = entry.achievementDefinition;
+                    return (
+                        <Timeline.Item key={entry.unlockedAt}
+                                       bullet={
+                                           <Avatar
+                                               size={32}
+                                               radius="xl"
+                                               src={`${API_URL}${achievementDefinition.icon}`}
+                                           />}>
+                            <Text color="dimmed"
+                                  size="sm">{entry.userName.value} unlocked <strong>{achievementDefinition.name}</strong> achievement</Text>
+                            <Text size="xs" mt={4}>
+                                {formatDistanceToNow(Date.parse(entry.unlockedAt), { addSuffix: true })}
+                            </Text>
+                        </Timeline.Item>
+                    );
+                })}
             </Timeline>
         </Container>
     )
--- utils/queries.js
+++ utils/queries.js
@@ -1,6 +1,6 @@
 export const API_URL = process.env.NEXT_PUBLIC_GH_STATS_API_URL;
 export const achievements = () => fetch(`${API_URL}/achievements`).then(response => response.json())
 export const achievementsStats = () => fetch(`${API_URL}/achievements/stats`).then(response => response.json())
-export const feed = () => fetch(`${API_URL}/achievements/last`).then(response => response.json())
+export const feed = () => fetch(`${API_URL}/achievements/feed`).then(response => response.json())
 export const scoreBoard = () => fetch(`${API_URL}/achievements/top`).then(response => response.json())
 export const userAchievements = (id) => fetch(`${API_URL}/users/${id}`).then(response => response.json())

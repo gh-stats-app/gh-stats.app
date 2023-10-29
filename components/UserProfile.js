7ab6f44a0e6f5ec0f84ccfdfe0b339d217ebf2a1
Bartosz Gałek <bartosz@galek.com.pl>
2023-10-29 17:56:29 +0100

    user profile api aligment

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,38 +1,42 @@
 import { useQuery } from "react-query";
 import { userAchievements } from "../utils/queries";
-import { Avatar, Container, Group, LoadingOverlay, Stack, Text } from "@mantine/core";
+import { Avatar, Container, Group, LoadingOverlay, Stack, Text, Anchor } from "@mantine/core";
 
 export default function UserProfile({ id }) {
     const { data, isLoading } = useQuery(['feed', id], () => userAchievements(id))
-    if (isLoading) return <LoadingOverlay visible/>;
+    if (isLoading) return <LoadingOverlay visible />;
     return (
         <Container size="xl">
             <Stack>
-                {data.map(entry => (
-                    <Group noWrap key={entry.id}>
-                        <Avatar src={"https://api.gh-stats.app" + entry.image} size={94} radius="md"/>
-                        <div>
-                            <Text size="xs" sx={{ textTransform: 'uppercase' }} weight={700} color="dimmed">
-                            </Text>
-
-                            <Text size="lg" weight={500}>
-                                {entry.name}
-                            </Text>
-
-                            <Group noWrap spacing={10} mt={3}>
-                                <Text size="xs" color="dimmed">
-                                    {entry.description}
+                {data.map(entry => {
+                    const achievement = entry.achievement;
+                    return (
+                        <Group noWrap key={achievement.id}>
+                            <Avatar src={"https://api.gh-stats.app" + achievement.image} size={94} radius="md" />
+                            <div>
+                                <Text size="xs" sx={{ textTransform: 'uppercase' }} weight={700} color="dimmed">
                                 </Text>
-                            </Group>
 
-                            <Group noWrap spacing={10} mt={5}>
-                                <Text size="xs" color="dimmed">
-                                    8c4a8d55c5dc605f70bc14b9fc176b38c27a2937
+                                <Text size="lg" weight={500}>
+                                    {achievement.name}
                                 </Text>
-                            </Group>
-                        </div>
-                    </Group>
-                ))}
+
+                                <Text size="xs" color="dimmed">
+                                    {achievement.description}
+                                </Text>
+
+                                <Text size="xs" color="dimmed">
+                                    <Anchor href={`https://github.com/search?q=${entry.commitId}&type=commits`} target="_blank">
+                                        {entry.commitId.substr(1, 16)}&hellip;
+                                    </Anchor>
+                                </Text>
+                                <Text size="xs" color="dimmed">
+                                    {entry.unlockedAt}
+                                </Text>
+                            </div>
+                        </Group>
+                    )
+                })}
 
             </Stack>
         </Container>
